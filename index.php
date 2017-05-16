9739651781514941b447c4d2ba24e002cb9bfa87
edsonlb <ti_edson@yahoo.com.br>
2017-05-16 21:29:58 +0200

    Just a test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 require "configuracao.php";
 require "utilitario.php"; 
 require "restclient.php"; 
-
+echo $api;
 $api = new RestClient(['base_url' => $api]);
 
 $bairros = $api->get("imovel", ['transform' => '1', 'token' => $token, 'busca'=>'BAIRROS']);
