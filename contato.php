737ddbb51c75ff780d33408934913b462fc8c339
edsonlb <ti_edson@yahoo.com.br>
2017-05-29 20:42:05 +0200

    Configurando Email do Site

--- contato.php
+++ contato.php
@@ -3,13 +3,13 @@
     require "configuracao.php";
     require "utilitario.php"; 
 
-    if ( empty($_POST['chave']) )
+    if ( empty($_POST['link']) )
         die('NAO PODE!');
 
     enviaEmail(
         trataString($_POST['email']), 
         trataString('CONTATO DE CLIENTE'), 
-        trataString($_POST['texto']), 
+        trataString($_POST['texto']).'<br>LINK: '.$_POST['link'], 
         trataString('edsonlb@gmail.com'), 
         trataString($imobiliaria_email));
 /*
