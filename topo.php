bca0ad6096aef932f58bd3d8553425376033a19b
edsonlb <ti_edson@yahoo.com.br>
2017-05-29 21:19:29 +0200

    Configurando Email do Site

--- topo.php
+++ topo.php
@@ -84,16 +84,16 @@
           </div>
 
           <div class="form-group">
-            <input type="text" name="nome" min="10" class="form-control input-lg" placeholder="Seu nome..." required="true">
+            <input type="text" name="nome" pattern=".{10,}" class="form-control input-lg" placeholder="Seu nome..." required="true">
           </div>
           <div class="form-group">
-            <input type="email" name="email" min="10" class="form-control input-lg" placeholder="Seu email..." required="true">
+            <input type="email" name="email" pattern=".{10,}" class="form-control input-lg" placeholder="Seu email..." required="true">
           </div>
           <div class="form-group">
-            <input type="tel" name="telefone" min="8" max="13" class="form-control input-lg" placeholder="Seu telefone..." required="true">
+            <input type="tel" name="telefone" pattern=".{8,13}" class="form-control input-lg" placeholder="Seu telefone..." required="true">
           </div>
           <div class="form-group">
-            <textarea name="texto" min="20" placeholder="Sua mensagem..." class="form-control input-lg" rows="3" required="true"></textarea>
+            <textarea name="texto" pattern=".{20,}" placeholder="Sua mensagem..." class="form-control input-lg" rows="3" required="true"></textarea>
           </div>
       </div>
       <div class="modal-footer">
