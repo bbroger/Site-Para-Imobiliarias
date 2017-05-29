cec902c81b488b9069ec42ca0ce84a25b3abd00f
edsonlb <ti_edson@yahoo.com.br>
2017-05-29 20:52:10 +0200

    Configurando Email do Site

--- contato.php
+++ contato.php
@@ -9,7 +9,7 @@
     enviaEmail(
         trataString($_POST['email']), 
         trataString('CONTATO DE CLIENTE'), 
-        trataString($_POST['texto']).'<br>LINK: '.$_POST['link'], 
+        '<b>EMAIL DO SITE. FAVOR RESPONDER AO CLIENTE. VERIFIQUE O ENDEREÇO DE EMAIL</b><br>LINK: '.$_POST['link'].'<br/><br/>'.trataString($_POST['texto']), 
         trataString('edsonlb@gmail.com'), 
         trataString($imobiliaria_email));
 
